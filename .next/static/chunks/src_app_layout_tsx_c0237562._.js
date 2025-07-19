9f8b8186adbfb5283e658f921adb72915ca64e63
samurthika <samurthikat@gmail.com>
2025-07-19 17:07:29 +0530

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1752925048213

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__883557c8._.css",
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/src_f83de7d7._.js"
+],
+    source: "dynamic"
+});
